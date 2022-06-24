e5eefa623dfe33e0bfb7777f9d9ad786bedd5a3a
Maxim Dmitriev <Renissance@tut.by>
2022-06-24 09:52:53 +0300

    Lesson 54

--- src/components/Users/UsersС.tsx
+++ src/components/Users/UsersС.tsx
@@ -11,13 +11,10 @@ type UserPropsType = {
 }
 
 class UsersC extends React.Component<UserPropsType, any> {
-    constructor(props: UserPropsType) {
-        super(props);
-
+    componentDidMount() {
         axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
             this.props.setUsers(response.data.items)
         });
-
     }
 
 
