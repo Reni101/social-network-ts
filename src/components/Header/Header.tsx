ad495f3932856ffbaab4607196579988e5e689e0
Maxim Dmitriev <Renissance@tut.by>
2022-07-25 09:58:24 +0300

    Lesson 63 TS

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -10,7 +10,6 @@ type PropsType= {
 }
 
 const Header = (props: PropsType) => {
-    debugger
     return (
         <div className={styleH.header}>
             <div><img className={styleH.logo} src={logo} alt="logo"/>
