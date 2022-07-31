0b6dbd3d572f0619715ea278e9a6737845d65409
Maxim Dmitriev <Renissance@tut.by>
2022-07-31 16:43:50 +0300

    Lesson 63 TS

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,9 +1,25 @@
 import axios from "axios";
 
+const instance = axios.create({
+    withCredentials: true,
+    headers: {
+        "API-KEY": "fab19197-098e-4362-876a-0c0797e21ac6"
+    }
+
+});
+
+const baseURL = 'https://social-network.samuraijs.com/api/1.0/'
+
 export const getUsers = (currentPage: number = 1, pageSize: number = 10) => {
-    return axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`, {
-        withCredentials: true
-    }).then(response => response.data)
+    return instance.get(baseURL + `users?page=${currentPage}&count=${pageSize}`)
+        .then(response => response.data)
 }
+
+export const followUser = (userId: number) => {
+    return instance.post(baseURL + `follow/${userId}`)
+        .then(response => response.data)
+}
+
+
 
 
