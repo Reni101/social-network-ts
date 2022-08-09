be01a3e72eb41967f513c033b3848743ef2a5683
Maxim Dmitriev <Renissance@tut.by>
2022-08-09 12:19:12 +0300

    Lesson 70 with TS

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import styleP from "../Profile.module.css";
 import Preloader from "../../Preloader";
 import {ProfileType} from "../../../Redux/profile-reducer";
+import ProfileStatus from "./ProfileStatus/ProfileStatus";
 
 type PropsType = {
     profile: ProfileType
 }
+
 
 const ProfileInfo = (props: PropsType) => {
     if (!props.profile) {
@@ -15,7 +17,8 @@ const ProfileInfo = (props: PropsType) => {
             <div>
                 <div>{props.profile.fullName}</div>
                 <img src={props.profile.photos.large} alt="Avatar"/>
-                <div className={styleP.item}>{props.profile.aboutMe}</div>
+               {/* <div className={styleP.item}>{props.profile.aboutMe}</div>*/}
+                <ProfileStatus/>
             </div>
         );
 };
---
+++ src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatus.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProfileStatus = (props:any) => {
+    return (
+        <div>
+<span>{props.}</span>
+        </div>
+    );
+};
+
+export default ProfileStatus;
