dc9dd5adc75de5cc1ca0f5e64b665eb609d7b32b
Maxim Dmitriev <Renissance@tut.by>
2022-08-11 18:35:07 +0300

    Lesson 75 without TS

--- src/components/Login/LoginForm.tsx
+++ src/components/Login/LoginForm.tsx
@@ -5,9 +5,24 @@ import {Field, reduxForm} from "redux-form";
 const LoginForm = () => {
     return (
         <form>
-            <div><Field placeholder={"Login"} component={"input"}/></div>
-            <div><Field placeholder={"Password"} component={"input"}/></div>
-            <div><input type={"checkbox"}/>remember me</div>
+            <div>
+                <Field placeholder={"Login"}
+                       component={"input"}
+                       name={"login"}/>
+            </div>
+            <div>
+                <Field
+                    placeholder={"Password"}
+                    component={"input"}
+                    name={"password"}/>
+            </div>
+            <div>
+                <Field
+                    type={"checkbox"}
+                    component={"input"}
+                    name={"rememberMe"}
+                />remember me
+            </div>
             <div>
                 <button> login</button>
             </div>
