e5ed4f324b465959358c11d36adb71956104a5a3
Maxim Dmitriev <Renissance@tut.by>
2022-08-11 18:19:08 +0300

    Lesson 75 without TS

--- src/components/Login/LoginPage.tsx
+++ src/components/Login/LoginPage.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import LoginForm from "./LoginForm";
 
 const LoginPage = () => {
     return (
         <div>
             <h1>LOGIN</h1>
+            <LoginForm/>
         </div>
     );
 };
