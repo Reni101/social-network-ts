68c8518496e98a2d4e1f88ea5df0d9205f3fe321
maksimdmitriev <Renissance@tut.by>
2023-02-09 10:29:28 +0300

    prettier

--- src/components/Dialogs/AddMessageForm.tsx
+++ src/components/Dialogs/AddMessageForm.tsx
@@ -1,49 +1,49 @@
-import React from 'react'
-import { useFormik } from 'formik'
-import { useDispatch } from 'react-redux'
-import { sendMessageAC } from '../../Redux/dialogs-reducer'
+import React from "react";
+import { useFormik } from "formik";
+import { useDispatch } from "react-redux";
+import { sendMessageAC } from "../../Redux/dialogs-reducer";
 
 type FormikType = {
-	Text?: string
-}
+  Text?: string;
+};
 
 export const AddMessageForm = () => {
-	const dispatch = useDispatch()
-	const formik = useFormik({
-		initialValues: {
-			Text: ''
-		},
-		onSubmit: values => {
-			dispatch(sendMessageAC({ messageBody: values.Text }))
-			formik.resetForm()
-		},
-		validate: (values: FormikType) => {
-			const errors: FormikType = {}
+  const dispatch = useDispatch();
+  const formik = useFormik({
+    initialValues: {
+      Text: "",
+    },
+    onSubmit: (values) => {
+      dispatch(sendMessageAC({ messageBody: values.Text }));
+      formik.resetForm();
+    },
+    validate: (values: FormikType) => {
+      const errors: FormikType = {};
 
-			if (!values.Text) {
-				errors.Text = 'Required'
-			}
-			return errors
-		}
-	})
+      if (!values.Text) {
+        errors.Text = "Required";
+      }
+      return errors;
+    },
+  });
 
-	return (
-		<form onSubmit={formik.handleSubmit}>
-			<div>
-				<textarea
-					placeholder='Send messages Formik'
-					name='Text'
-					onChange={formik.handleChange}
-					value={formik.values.Text}
-				></textarea>
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <div>
+        <textarea
+          placeholder="Send messages Formik"
+          name="Text"
+          onChange={formik.handleChange}
+          value={formik.values.Text}
+        ></textarea>
 
-				{formik.touched.Text && formik.errors.Text && (
-					<div style={{ color: 'red' }}>{formik.errors.Text}</div>
-				)}
-			</div>
-			<div>
-				<button type='submit'>Send message</button>
-			</div>
-		</form>
-	)
-}
+        {formik.touched.Text && formik.errors.Text && (
+          <div style={{ color: "red" }}>{formik.errors.Text}</div>
+        )}
+      </div>
+      <div>
+        <button type="submit">Send message</button>
+      </div>
+    </form>
+  );
+};
--- src/components/Dialogs/DialogsContainer.tsx
+++ src/components/Dialogs/DialogsContainer.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { Dialogs } from './Dialogs'
-import { useSelector } from 'react-redux'
-import { AppRootStateType } from '../../Redux/Redux-store'
-import { Navigate } from 'react-router-dom'
+import React from "react";
+import { Dialogs } from "./Dialogs";
+import { useSelector } from "react-redux";
+import { AppRootStateType } from "../../Redux/Redux-store";
+import { Navigate } from "react-router-dom";
 
 export const DialogsContainer = () => {
-	const isAuth = useSelector<AppRootStateType>(state => state.auth.isAuth)
-	if (!isAuth) {
-		return <Navigate to={'/login'} />
-	}
-	return (
-		<>
-			<h2>In progress...</h2>
-			<Dialogs />
-		</>
-	)
-}
+  const isAuth = useSelector<AppRootStateType>((state) => state.auth.isAuth);
+  if (!isAuth) {
+    return <Navigate to={"/login"} />;
+  }
+  return (
+    <>
+      <h2>In progress...</h2>
+      <Dialogs />
+    </>
+  );
+};
